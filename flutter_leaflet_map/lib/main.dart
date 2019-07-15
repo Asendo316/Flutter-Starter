7dfb85e5fc39750b8cca20eb9102ffb49ca303d0
IBIKUNLE, Adeoluwa A <a.ibikunle@innovectives.com>
2019-07-15 14:14:35 +0100

    Added Flutter Maps

--- flutter_leaflet_map/lib/main.dart
+++ flutter_leaflet_map/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,9 +25,36 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
-
+      appBar: new AppBar(
+        title: new Text("Leaflet Maps"),
+      ),
+      body: new FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(40.71, -74.00),
+          minZoom: 10.0,
+        ),
+        layers: [
+          new TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c']),
+          new MarkerLayerOptions(markers: [
+            new Marker(
+                width: 45.0,
+                height: 45.0,
+                point: new LatLng(40.71, -74.12),
+                builder: (context) => new Container(
+                      child: IconButton(
+                          icon: Icon(Icons.location_on),
+                          color: Colors.red,
+                          iconSize: 45.0,
+                          onPressed: () {
+                            print("Marker Tapped");
+                          }),
+                ))
+          ])
+        ],
+      ),
     );
   }
 }
