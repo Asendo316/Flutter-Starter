b1d3f5a22ea20e52ea4bb1e2a1b1a208101b0041
IBIKUNLE, Adeoluwa A <a.ibikunle@innovectives.com>
2019-07-16 17:08:57 +0100

    Pending animations

--- flutter_animations/lib/main.dart
+++ flutter_animations/lib/main.dart
@@ -22,7 +22,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
-  Animation animation;
+  Animation animation, delayedAnimation, muchDelayedAnimation;
   AnimationController animationController;
 
   @override
@@ -30,11 +30,19 @@ class _MyHomePageState extends State<MyHomePage>
     super.initState();
     animationController = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 5),
     );
 
     animation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
         parent: animationController, curve: Curves.fastOutSlowIn));
+
+    delayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn)));
+
+    muchDelayedAnimation = Tween(begin: -1.0, end: 0.0).animate(CurvedAnimation(
+        parent: animationController,
+        curve: Interval(0.8, 1.0, curve: Curves.fastOutSlowIn)));
 
     animationController.forward();
   }
@@ -47,55 +55,91 @@ class _MyHomePageState extends State<MyHomePage>
       animation: animationController,
       builder: (BuildContext context, Widget child) {
         return Scaffold(
-          body: Transform(
-            transform:
-                Matrix4.translationValues(animation.value * width, 0.0, 0.0),
-            child: new Center(
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          body: Align(
+            alignment: Alignment.center,
+            child: Container(
+              child: new Center(
+                child: new ListView(
+                  shrinkWrap: true,
                   children: <Widget>[
-                    new Text("Login",
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.bold)),
-                    new SizedBox(
-                      height: 20.0,
-                    ),
-                    new TextField(
-                        decoration: new InputDecoration(
-                      hintText: "Username",
-                    )),
-                    new SizedBox(
-                      height: 20.0,
-                    ),
-                    new TextField(
-                        decoration: new InputDecoration(
-                      hintText: "Password",
-                    )),
-                    new SizedBox(
-                      height: 20.0,
-                    ),
-                    new RaisedButton(
-                      onPressed: () {},
-                      color: Colors.lightBlue,
-                      child: Text(
-                        "Login",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    new SizedBox(
-                      height: 20.0,
-                    ),
-                    new Text("Don\'t have and account ?",
-                        style: TextStyle(
-                            fontSize: 20.0)),
-                    new SizedBox(
-                      height: 20.0,
-                    ),
-                    new OutlineButton(onPressed: (){},
-                      child: Text("Signup"),
-                    color: Colors.lightGreen,)
+                    Transform(
+                        transform: Matrix4.translationValues(
+                            animation.value * width, 0.0, 0.0),
+                        child: new Center(
+                          child: Container(
+                            padding: EdgeInsets.all(20.0),
+                            child: new Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new Text("Login",
+                                      style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold)),
+                                  new SizedBox(
+                                    height: 20.0,
+                                  )
+                                ]),
+                          ),
+                        )),
+                    Transform(
+                        transform: Matrix4.translationValues(
+                            delayedAnimation.value * width, 0.0, 0.0),
+                        child: new Center(
+                          child: Container(
+                            padding: EdgeInsets.all(20.0),
+                            child: new Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new TextField(
+                                      decoration: new InputDecoration(
+                                    hintText: "Username",
+                                  )),
+                                  new SizedBox(
+                                    height: 20.0,
+                                  ),
+                                  new TextField(
+                                      decoration: new InputDecoration(
+                                    hintText: "Password",
+                                  )),
+                                  new SizedBox(
+                                    height: 20.0,
+                                  ),
+                                ]),
+                          ),
+                        )),
+                    Transform(
+                        transform: Matrix4.translationValues(
+                            muchDelayedAnimation.value * width, 0.0, 0.0),
+                        child: new Center(
+                          child: Container(
+                            padding: EdgeInsets.all(20.0),
+                            child: new Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  new RaisedButton(
+                                    onPressed: () {},
+                                    color: Colors.lightBlue,
+                                    child: Text(
+                                      "Login",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                  new SizedBox(
+                                    height: 20.0,
+                                  ),
+                                  new Text("Don\'t have and account ?",
+                                      style: TextStyle(fontSize: 20.0)),
+                                  new SizedBox(
+                                    height: 20.0,
+                                  ),
+                                  new OutlineButton(
+                                    onPressed: () {},
+                                    child: Text("Signup"),
+                                    color: Colors.lightGreen,
+                                  )
+                                ]),
+                          ),
+                        )),
                   ],
                 ),
               ),
